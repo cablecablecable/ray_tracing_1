3280a4ebf663b4d0fbd2236a640f1e16b21659a1
David <182460752+cablecablecable@users.noreply.github.com>
2025-09-22 21:35:03 -0400

    workin

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
+#include <complex>
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+
+    if (discriminant >= 0)
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+
+    return -1.0;
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
+    double t = hit_sphere(point3(0,0,-1), 0.5, r);
+
+    if (t >= 0)
+    {
+        point3 intersection = r.at(t);
+        vec3 normal = vec3(intersection - point3(0,0,-1));
+        normal = unit_vector(normal);
+
+        return 0.5 * color(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     double a = 0.5 * (unit_direction.y() + 1.0);
